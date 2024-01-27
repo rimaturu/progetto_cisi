afc290e41913592e737c90184930b721f6777afd
JudJag <lucageppo@gmail.com>
2024-01-27 13:46:00 +0100

    Update for Relazione

--- ISI/Parametri.m
+++ ISI/Parametri.m
@@ -13,14 +13,13 @@ r = 0.2;   % [m]
 
 g = 9.81;
 
-params = [m, c_a, J, L, l, h, r, g];
-params_incert = [m + normrnd(400, 100), c_a - normrnd(1, 0.2), J + normrnd(500, 50), L, l, h, r, g];
-% params_incert = params;
+params = [m, c_a, J, L, l, h, r, g];     % parametri filtri
+params_incert = [m + normrnd(400, 100), c_a - normrnd(1, 0.2), J + normrnd(500, 50), L, l, h, r, g]; % parametri sistema reale
 
 % Condizioni Iniziali %
 F_0 = 1500;
 dq_0 = [0; 0];
-q_0 = [0; -pi/6];
+q_0 = [0; 0];
 
 
 %% parametri sensori
@@ -31,36 +30,38 @@ laserd1_accuracy = 3;          % [mm]
 
 laserd1_params = [laserd1_sample_rates, laserd1_resolution, laserd1_accuracy];
 
+
 % sensor laser D:
 % https://www.acuitylaser.com/wp-content/uploads/ar3000-datasheet.pdf
-
 laserD_sample_rates = 1/100; % [50 Hz]
 laserD_resolution = 1;       % [mm]
-laserD_accuracy = 20;        % [mm]    % TODO:check if +-20 mm is the dev std or the 95perc of confidence [in this case we need to consider accuracy/3]
+laserD_accuracy = 20;        % [mm]    
 
 laserD_params = [laserD_sample_rates, laserD_resolution, laserD_accuracy];
+
 
 % relative encoder omega:
 encoder_accuracy = 2*pi/1200; % [rad]
 encoder_resolution = 2*pi/1200; % [rad/pulse]
 encoder_sample_rates = 1/50;    % [s]
+
 encoder_params = [r, encoder_accuracy, encoder_resolution, encoder_sample_rates];
 
 
-% define the slowest sample rate (min freq == max period) of all the sensors to set up the zero order hold block
-sample_rates = [encoder_sample_rates, laserD_sample_rates, laserd1_sample_rates]; % sample rates of all the sensors period
-min_sample_rates = max(sample_rates); % slowest one
+% Ricavo la frequenza minima di funzionamento tra i sensori (min freq == max period) 
+sample_rates = [encoder_sample_rates, laserD_sample_rates, laserd1_sample_rates]; % Tempo di campionamento di tutti i sensori
+min_sample_rates = max(sample_rates); % Il più lento
 
 
-q_hat_in = zeros(4,1);
-P_k_in = eye(4);
 
+% Matrice covarianza del rumore del modello di osservazione (sensori)
 R_k = diag([(laserD_accuracy/1000)^2, (laserd1_accuracy/1000)^2, (encoder_accuracy/encoder_sample_rates)^2]);
 
-q_hat_0 = [ dq_0(1) + normrnd(0, 0.1);
-            dq_0(2) + normrnd(0, 0.1);
-            q_0(1) + normrnd(0, 0.1);
-            q_0(2) + normrnd(0, pi/100)];
+% Condizione iniziale filtri Kalman (q_1|0) 
+q_hat_0 = [ normrnd(dq_0(1), 0.1);
+            normrnd(dq_0(2), 0.1);
+            normrnd(q_0(1), 0.5);
+            normrnd(q_0(2), pi/100)];
 
 
 % Faccio partire la simulazione su Simulink
